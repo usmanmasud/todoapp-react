96aeaf3ae6b2d2405fc703593b8b9ad60ae16c52
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2024-09-13 23:02:10 +0100

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Header from "./components/Header";
 import Todo from "./components/Todo";
+import './App.css'
 
 
 export default function App() {
   return <div className="Appp">
+    <Header />
     <Todo />
   </div>;
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import TodoItem from "./TodoItem";
 
 export default function Todo() {
     const [todo, setTodo] = useState('');
@@ -15,7 +16,7 @@ export default function Todo() {
                 <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
                 <button type="submit">Submit</button>
             </form>
-            {todos.map(item => <h3>{item}</h3>)}
+            {todos.map(item => <TodoItem key={item} item={item} />)}
         </div>
     )
 }
